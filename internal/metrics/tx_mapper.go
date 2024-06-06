3a336d3dba83918bb24fbda725c669ff421c06aa
faheelsattar <faheelsattar@yahoo.com>
2024-06-07 03:09:17 +0500

    add tx mapper

---
+++ internal/metrics/tx_mapper.go
@@ -0,0 +1,54 @@
+package metrics
+
+import "sync"
+
+type TxMetrics interface {
+	AddEncryptedTx(identity string, encryptedTx []byte)
+	AddDecryptionData(identity string, dd *DecryptionData)
+}
+
+type DecryptionData struct {
+	Key  []byte
+	Slot uint64
+}
+
+type Tx struct {
+	EncryptedTx []byte
+	DD          *DecryptionData
+}
+
+type TxMapper struct {
+	Data  map[string]*Tx
+	mutex sync.Mutex
+}
+
+func NewTxMapper() *TxMapper {
+	return &TxMapper{
+		Data:  make(map[string]*Tx),
+		mutex: sync.Mutex{},
+	}
+}
+
+func (tm *TxMapper) AddEncryptedTx(identity string, encryptedTx []byte) {
+	tm.mutex.Lock()
+	defer tm.mutex.Unlock()
+
+	tx, exists := tm.Data[identity]
+	if !exists {
+		tx = &Tx{}
+		tm.Data[identity] = tx
+	}
+	tx.EncryptedTx = encryptedTx
+}
+
+func (tm *TxMapper) AddDecryptionData(identity string, dd *DecryptionData) {
+	tm.mutex.Lock()
+	defer tm.mutex.Unlock()
+
+	tx, exists := tm.Data[identity]
+	if !exists {
+		tx = &Tx{}
+		tm.Data[identity] = tx
+	}
+	tx.DD = dd
+}
