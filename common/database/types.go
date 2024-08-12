32e1583d79ff8b7d34b5b326971bf6dd876f4f6b
faheelsattar <faheelsattar@yahoo.com>
2024-08-12 18:24:59 +0500

    clean code

--- common/database/types.go
+++ common/database/types.go
@@ -1,15 +1,22 @@
 package database
 
-import "github.com/jackc/pgx/v5/pgtype"
+import (
+	"math"
 
-func Uint8ToPgTypeInt8(data int64) pgtype.Int8 {
-	return pgtype.Int8{Int64: data, Valid: true}
-}
+	"github.com/jackc/pgx/v5/pgtype"
+)
 
+// This utility function convert uint64 to pgtype.Int8.
+// In the case if data overflows it returns Valid false
+// which means its null in postgres types
 func Uint64ToPgTypeInt8(data uint64) pgtype.Int8 {
+	if data > math.MaxInt64 {
+		return pgtype.Int8{Int64: 0, Valid: false}
+	}
 	return pgtype.Int8{Int64: int64(data), Valid: true}
 }
 
+// This utility function convert bool to pgtype.Bool.
 func BoolToPgTypeBool(data bool) pgtype.Bool {
 	return pgtype.Bool{Bool: data, Valid: true}
 }
