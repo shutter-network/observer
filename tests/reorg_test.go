66b903f59dd9099fe5f459bd4e4550a361e60a2f
faheelsattar <faheelsattar@yahoo.com>
2025-04-08 10:45:11 +0200

    add validator registry reorg test

--- tests/reorg_test.go
+++ tests/reorg_test.go
@@ -49,3 +49,45 @@ func (s *TestMetricsSuite) TestHandlePotentialReorgTXSubmittedEvent() {
 	s.Require().Equal(int64(89), syncStatus.BlockNumber)
 	s.Require().Empty(syncStatus.BlockHash)
 }
+
+func (s *TestMetricsSuite) TestHandlePotentialReorgValidatorRegistry() {
+	ctx := context.Background()
+	// Setup initial sync status
+	blockHash := [32]byte{1, 2, 3}
+	err := s.dbQuery.CreateValidatorRegistryEventsSyncedUntil(ctx, data.CreateValidatorRegistryEventsSyncedUntilParams{
+		BlockHash:   blockHash[:],
+		BlockNumber: 99,
+	})
+	s.Require().NoError(err)
+
+	// Create header with matching parent hash
+	header := &types.Header{
+		Number:     big.NewInt(100),
+		ParentHash: blockHash,
+	}
+
+	// no reorg
+	err = s.validatorRegistrySyncer.HandlePotentialReorg(ctx, header)
+	s.Require().NoError(err)
+
+	blockHash = [32]byte{1, 2, 3}
+	err = s.dbQuery.CreateValidatorRegistryEventsSyncedUntil(ctx, data.CreateValidatorRegistryEventsSyncedUntilParams{
+		BlockHash:   blockHash[:],
+		BlockNumber: 99,
+	})
+	s.Require().NoError(err)
+
+	header = &types.Header{
+		Number:     big.NewInt(100),
+		ParentHash: [32]byte{4, 5, 6},
+	}
+
+	// reorg
+	err = s.validatorRegistrySyncer.HandlePotentialReorg(ctx, header)
+	s.Require().NoError(err)
+
+	syncStatus, err := s.dbQuery.QueryValidatorRegistryEventsSyncedUntil(ctx)
+	s.Require().NoError(err)
+	s.Require().Equal(int64(89), syncStatus.BlockNumber)
+	s.Require().Empty(syncStatus.BlockHash)
+}
